f46e622e32ddec8ca8ed844dbb44f22a0a2b8972
Cristiano Marques <cristianoms225@gmail.com>
2022-11-30 13:57:21 -0300

    Update script-11-3.js

--- script-11-3.js
+++ script-11-3.js
@@ -1,7 +1,11 @@
 
-let flow = {receitas:[],
-	    despesas:[] }
-funtion financialFlow(receitas[],despesas[]){
+let family = {
+	receitas:[2500, 3200, 250.43, 360.45],
+	despesas:[320.34, 128.45,176.87, 1450] }
+funtion financialFlow(){
+	...estrutura de repeticao....
+	for (i=0;i<family.length;i++){
+	}
 	total = receitas[] - despesas[]
 	return total
 }
