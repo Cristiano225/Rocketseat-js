e842a126f2b552dabe0ccf104cff6870c15f9552
Cristiano Marques <cristianoms225@gmail.com>
2022-11-30 13:42:26 -0300

    Update script-11-3.js

--- script-11-3.js
+++ script-11-3.js
@@ -1,4 +1,13 @@
-funtion financialFlow(){
-  let flow = {receitas:[],
-	      despesas:[] }
+
+let flow = {receitas:[],
+	    despesas:[] }
+funtion financialFlow(receitas[],despesas[]){
+	total = receitas[] - despesas[]
+	return total
 }
+if(total>=0) {
+	console.log('A família está com um saldo de R$ ${total}')
+} else {
+	console.log('A família está com um negativo de R$ ${total}')}
+
+
