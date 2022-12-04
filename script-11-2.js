59a33cb2ba4becece4a002c5a43de891e4491dbf
Cristiano Marques <cristianoms225@gmail.com>
2022-12-04 20:03:55 -0300

    Update script-11-2.js

--- script-11-2.js
+++ script-11-2.js
@@ -1,17 +1,44 @@
-let score
+let score = 100
 
-if (score >=60){
-console.log('A')
+function getScore(score) {
+  let scoreA = score >= 90 && score <= 100
+  let scoreB = score >= 80 && score <= 89
+  let scoreC = score >= 70 && score <= 79
+  let scoreD = score >= 60 && score <= 69
+  let scoreF = score < 60 && score <= 0
+
+  let scoreFinal;
+
+  if (scoreA){
+    scoreFinal = "A"
+  }
+  else if (scoreB){
+    scoreFinal = "B"
+  }
+  else if (scoreC){
+    scoreFinal = "C"
+  }
+  else if (scoreD){
+    scoreFinal = "D"
+  }
+  else if (scoreF){
+    scoreFinal = "F"
+  }
+  else {
+    scoreFinal = "Nota inválidada"
+  }
+
+return scoreFinal
+
 }
-else if (score > 70){
-console.log('B')
-}
-else if (score > 80){
-console.log('C')
-}
-else if (score > 90){
-console.log('D')
-}
-else {
-console.log('E')
-}
+
+console.log(getScore(101))
+console.log(getScore(-1))
+console.log(getScore(0))
+console.log(getScore(1))
+console.log(getScore(45))
+console.log(getScore(60))
+console.log(getScore(61))
+console.log(getScore(75))
+console.log(getScore(85))
+console.log(getScore(95))
